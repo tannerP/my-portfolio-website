e75a733a629d01a0ff393b22af3fed33246fafdf
Tanner Phan <tannerphan@gmail.com>
2017-05-05 13:08:31 -0700

    add footer, and work on content_viewer tag"

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,27 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @HostListener('scroll', ['$event'])
-  title = 'app works!';
+  public navIsFixed = false;
+  public  title = 'app works!';
+
+  constructor(@Inject(DOCUMENT) private document: any) { }
+
+  ngOnInit() { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    let number = this.document.body.scrollTop;
+    if (number > 120) {
+      this.navIsFixed = true;
+      console.log("number " + number);
+    } else if (this.navIsFixed && number < 10) {
+      console.log("number " + number);
+      this.navIsFixed = false;
+    }
+  }
+  log(event: any): void {
+    console.log(this.document.body.scroll)
+    console.log(event);
+  }
 }
+
