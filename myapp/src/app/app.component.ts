4300aaf22871c19710245769420ad3ba6f848f10
Tanner <tannerphan@gmail.com>
2017-05-07 02:04:24 -0700

    Added oranagdark. 's house

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,33 +1,61 @@
 import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('heroState', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
+      state('active',   style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
-  public navIsFixed = false;
+  private _state = false;
+  public state = ' ';
   public  title = 'app works!';
-
   constructor(@Inject(DOCUMENT) private document: any) { }
 
   ngOnInit() { }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    let number = this.document.body.scrollTop;
-    if (number > 120) {
-      this.navIsFixed = true;
-      console.log("number " + number);
-    } else if (this.navIsFixed && number < 10) {
-      console.log("number " + number);
-      this.navIsFixed = false;
-    }
+    // let number = this.document.body.scrollTop;
+    // if (number > 120) {
+    //   this.navIsFixed = true;
+    //   console.log("number " + number);
+    // } else if (this.navIsFixed && number < 10) {
+    //   console.log("number " + number);
+    //   this.navIsFixed = false;
+    // }
   }
-  log(event: any): void {
-    console.log(this.document.body.scroll)
+
+  onClick(event: any): void {
     console.log(event);
+    this._state = !this._state;
+    if(this._state === true){
+      this.state = 'active'
+    }
+    else{
+      this.state = 'inactive'
+    }
   }
 }
 
