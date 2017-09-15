9a9ec64b389be76120460ddb0aca27ccaa7910b9
Tanner Phan <tannerphan@gmail.com>
2017-09-15 15:10:26 -0700

    work on blog

--- myapp/src/app/blog/blog.component.ts
+++ myapp/src/app/blog/blog.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
-
+  private projects = [
+    {
+      name: 'BittyCasting',
+      length: '7 months',
+      projectType: 'Software Startup',
+      path: '../../assets/medias/bittycasting_mock.png',
+    },
+    {
+      name: 'Lane Detection',
+      length: '2 week',
+      projectType: 'Udacity - Self Driving Car ND',
+      path: '../../assets/medias/selfDrivingCar_blakwhite.png',
+    },
+    {
+      name: 'About Tannerphan.me',
+      length: 'Since 2017',
+      projectType: 'Personal Portfolio',
+      path: '../../assets/medias/sketchapp_background.png',
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
--- myapp/src/app/main/main.component.ts
+++ myapp/src/app/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MenuStateService } from '../services/menu-state.service';
 import { IgService } from '../services/ig.service';
 import { DOCUMENT } from '@angular/platform-browser';
+import { Router } from '@angular/router'
 import {trigger, state, style, animate, transition} from '@angular/animations';
 @Component({
   selector: 'app-main',
@@ -13,39 +14,19 @@ export class MainComponent implements OnInit {
   public menu_state: String;
   public menu_items = ['about', 'bio', 'people'];
   public bckgrnd_img_src = ['reads', '', 'photos', '',  'blog',  '', 'socials', '', 'portfolio'];
-  constructor(@Inject(DOCUMENT) private document: any, private ig: IgService, public menuService: MenuStateService) { }
-  public img_src = '../../assets/medias/me.jpg';
-  //  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    // let number = this.document.body.scrollTop;
-    // if (number > 120) {
-    //   this.navIsFixed = true;
-    //   console.log("number " + number);
-    // } else if (this.navIsFixed && number < 10) {
-    //   console.log("number " + number);
-    //   this.navIsFixed = false;
-    // }
-  }
+  private img_src = '../../assets/medias/me.jpg';
+  constructor(@Inject(DOCUMENT) private document: any,
+              private ig: IgService,
+              private  router: Router,
+              public menuService: MenuStateService) { }
 
-  ngOnInit() {
+  ngOnInit(): any{
     this.menu_state =  'about';
   }
-
-  menu_onClick(event: any, item): void {
-    // console.log(this.menu_state);
-    // console.log(item);
-    this.menuService.setState(item);
-    this.menu_state = this.menuService.getState();
-    // console.log(this.ig.httpLoadPics());
-    return;
+  click_portfolio(): any {
+    this.router.navigateByUrl('blog');
   }
-  monolog_pane_showorhide(id): String {
-    return id === this.menu_state ? '' : 'none';
-  }
-  getMenuState(): String {
-    return this.menuService.getState();
-  }
-  setStyle(): any {
+ setStyle(): any {
     let random: number;
     const url = ['../../assets/medias/me.jpg',
       '../../assets/medias/me.jpg',
@@ -55,7 +36,7 @@ export class MainComponent implements OnInit {
     const style = {
         'background-image': 'url(' + url[random] + ')',
         'background-position': 'center',
-        'background-repeat':'no-repeat',
+        'background-repeat': 'no-repeat',
         'background-size': 'contain',
         'height': '100px',
     }
